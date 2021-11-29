b2e53584db84ce68740077f971f360fd8d544ad7
Sasha O'Donovan <sasha.odonovan@gmail.com>
2021-11-29 08:51:39 +0000

    added working test file

---
+++ morse.test.js
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { translateToEnglish } from "./script";
+
+it("should return a translation from .- to A", () => {
+  const output = translateToEnglish(" .-");
+  expect(output).toBe("A");
+});
+
+it("should return a translation from -... to B", () => {
+  const output = translateToEnglish(" -...");
+  expect(output).toBe("B");
+});
+
+// export const translateToEnglish = (morse) => {
+//   const arrayForConversion = morse.split(" ").map((x) => {
+//     return englishCode[x];
+//   });
+//   const englishTranslation = arrayForConversion.join("");
+//   return englishTranslation;
+// };
